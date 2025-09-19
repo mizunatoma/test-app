c4b2e7b37586222b503c4c2009573c87ac82011c
mizunatoma <k40117135@gmail.com>
2025-09-19 22:25:06 +0900

    課題2の実装

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from "react";
 import { posts } from "./data/posts";
+import { Routes, Route, Link } from "react-router-dom";
+import PostDetail from "./components/PostDetail";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-center my-6">Blog</h1>
-      {posts.map((post) => (
-        <div key={post.id} className="border border-gray-300 m-4 p-4">
-          <p className="text-sm text-gray-500">
-            {new Date(post.createdAt).toLocaleDateString()}
-          </p>
-          <h2 className="text-xl font-semibold text-gray-800 mt-2">
-            {post.title}
-          </h2>
-          <p className="text-blue-600 text-sm">{post.categories.join(", ")}</p>
-          <div
-            className="mt-2 text-gray-700 line-clamp-2"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          />
-        </div>
-      ))}
-    </>
+    <div className="App">
+      <Link to="/" className="text-3xl font-bold my-6 p-6">
+        Blog
+      </Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              {posts.map((post) => (
+                <div key={post.id} className="border border-gray-300 m-4 p-4">
+                  <div className="flex justify-between items-center mb-4">
+                    <p className="text-sm text-gray-500">
+                      {new Date(post.createdAt).toLocaleDateString()}
+                    </p>
+                    <div className="flex gap-2">
+                      {post.categories.map((cat, index) => (
+                        <span
+                          key={index}
+                          className="px-3 py-1 text-sm border border-blue-500 text-blue-600 rounded"
+                        >
+                          {cat}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                  <Link
+                    to={`/posts/${post.id}`}
+                    className="text-x1 font-semibold text-gray-800 mt-2 hover:underline"
+                  >
+                    {post.title}
+                  </Link>
+                  <div
+                    className="mt-2 text-gray-700 line-clamp-2"
+                    dangerouslySetInnerHTML={{ __html: post.content }}
+                  />
+                </div>
+              ))}
+            </div>
+          }
+        />
+
+        <Route path="/posts/:id" element={<PostDetail />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/PostDetail.jsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { posts } from "../data/posts";
+
+export default function PostDetail() {
+    const { id } = useParams();
+    const post = posts.find((post) => post.id === Number(id));
+    
+    if (!post) {
+        return <p className="text-red-600">記事が見つかりません</p>;
+    }
+
+    return (
+        <article className="max-w-3x1 mx-auto p-6">
+            <img
+              src="https://placehold.jp/800x400.png"
+              alt="{post.title}"
+              className="w-full rounded-lg mb-6"
+            />
+            <div className="flex justify-between items-center mb-6">    
+                <p className="text-sm text-gray-500">
+                {new Date(post.createdAt).toLocaleDateString()}
+                </p>
+                <div className="flex gap-2">
+                {post.categories.map((cat, index) => (
+                    <span
+                    key={index}
+                    className="px-3 py-1 text-sm border border-blue-500 text-blue-600 rounded"
+                    >
+                        {cat}
+                    </span>
+                    ))}
+                </div>
+            </div>
+            <h1 className="text-3x1 font-bold mb-4">
+                {post.title}
+            </h1>
+            <div
+              className="prose max-w-none text-gray-700 leading-relaxed"
+              dangerouslySetInnerHTML={{__html: post.content}}
+            />
+        </article>
+    );
+}
